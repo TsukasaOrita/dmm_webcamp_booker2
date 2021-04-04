ea0cdf8a8a963d04670088020c3143f96c36079b
EC2 Default User <tsukasa680@gmail.com>
2021-04-04 11:14:57 +0000

    [finish]

---
+++ booker2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '0cef22fb6f7e2ed19e439ddbf29dc09d8072699bfe4374190d8e8f08f835b468ca18e900e18c48d5d14b11bfa80bfdc8d0b993939cd0faa3c007901fd2c2d474'
